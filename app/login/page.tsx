c0bdab1a725a4e5cb25a36cfd1d6695cd3410b0b
Guilherme Pereira <guilherme.borges@xplanning.com.br>
2024-11-17 18:10:20 -0300

    style: login page styling

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -10,19 +10,26 @@ const LoginPage = async () => {
     redirect("/");
   }
   return (
-    <div className="h-full">
-      <div className="mx-auto flex h-full max-w-[550px] flex-col justify-center p-8">
-        <h1 className="mb-3 text-4xl font-bold text-primary">Bem-vindo</h1>
-        <p className="mb-8 text-muted-foreground">
-          A SmartFin é uma plataforma de gestão financeira que utiliza IA para
-          monitorar suas movimentações, e oferecer insights personalizados,
-          facilitando o controle do seu orçamento.
+    <div className="flex h-full items-center">
+      <div className="mx-auto my-auto h-[500px] max-w-[550px] rounded-lg bg-gradient-to-r from-orange-400 to-orange-600 py-32 shadow-2xl">
+        <h1 className="mb-3 text-center text-6xl font-bold text-orange-50">
+          Bem-vindo
+        </h1>
+        <p className="mx-16 mb-8 py-4 text-center text-muted-foreground text-orange-50">
+          A <strong>SmartFin</strong> é uma plataforma de gestão financeira que
+          utiliza IA para monitorar suas movimentações, e oferecer insights
+          personalizados, facilitando o controle do seu orçamento.
         </p>
         <SignInButton>
-          <Button className="bg-primary" variant="outline">
-            <LogInIcon className="mr-2" />
-            Fazer login ou criar conta
-          </Button>
+          <div className="mx-16">
+            <Button
+              className="w-full bg-gradient-to-r from-orange-400 to-orange-600 text-orange-50 hover:text-orange-100"
+              variant="outline"
+            >
+              <LogInIcon className="mr-2" />
+              Fazer login ou criar conta
+            </Button>
+          </div>
         </SignInButton>
       </div>
     </div>
