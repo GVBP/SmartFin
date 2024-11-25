cd92612c20889106a5c63670a463407e0713ccc0
Guilherme Pereira <guilherme.borges@xplanning.com.br>
2024-11-25 01:51:22 -0300

    refactor: use let or const instead of var

--- app/_lib/prisma.ts
+++ app/_lib/prisma.ts
@@ -1,21 +1,17 @@
 /* eslint-disable no-unused-vars */
 import { PrismaClient } from "@prisma/client";
 
-declare global {
-  // eslint-disable-next-line no-var
-  var cachedPrisma: PrismaClient;
-}
-
+let cachedPrisma: PrismaClient | undefined;
 // open the database connection
 let prisma: PrismaClient;
 if (process.env.NODE_ENV === "production") {
   prisma = new PrismaClient();
 } else {
   // for the development environment, keep only one connection open
-  if (!global.cachedPrisma) {
-    global.cachedPrisma = new PrismaClient();
+  if (!cachedPrisma) {
+    cachedPrisma = new PrismaClient();
   }
-  prisma = global.cachedPrisma;
+  prisma = cachedPrisma;
 }
 
 export const db = prisma;
